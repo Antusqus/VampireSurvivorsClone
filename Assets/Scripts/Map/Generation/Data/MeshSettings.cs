9943d824f2d850b124ee544b8cb0ddf6b3e2a1e9
Anthony Quach <ta.quach@hotmail.com>
2025-01-24 11:28:12 +0100

    Terrain Generation tests

---
+++ Assets/Scripts/Map/Generation/Data/MeshSettings.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class MeshSettings : UpdatableData
+{
+
+    public const int numSupportedLOD = 5;
+    public const int numSupportedChunkSizes = 9;
+    public const int numSupportedFlatshadedChunkSizes = 3;
+    public static readonly int[] supportedChunkSizes = { 48, 72, 96, 120, 144, 168, 192, 216, 240 };
+
+    public float meshScale = 2.5f;
+    public bool useFlatShading;
+
+    [Range(0, numSupportedChunkSizes - 1)]
+    public int chunkSizeIndex;
+
+    [Range(0, numSupportedFlatshadedChunkSizes - 1)]
+    public int flatShadedChunkSizeIndex;
+
+    // Number of vertices per line of mesh rendered at LOD=0.
+    // This number includes the two extra vertices we create for calculating normals (which are excluded from final mesh)
+    public int numVertsPerLine
+    {
+        get
+        {
+            return supportedChunkSizes[(useFlatShading) ? flatShadedChunkSizeIndex : chunkSizeIndex] + 5;
+        }
+    }
+
+    public float meshWorldSize
+    {
+        get
+        {
+            return (numVertsPerLine - 3) * meshScale;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Map/Generation/Data/TextureData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class TextureData : UpdatableData
+{
+    // DEPRECATED, USE URP SHADER TECH
+    //public void ApplyToMaterial(Material material)
+    //{
+
+    //}
+
+    //public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
+    //{
+    //    material.SetFloat("minHeight", minHeight);
+    //    material.SetFloat("maxHeight", maxHeight);
+
+    //}
+}
