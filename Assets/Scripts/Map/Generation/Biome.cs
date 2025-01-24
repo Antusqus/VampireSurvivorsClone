94fd00e69608bb9a569f6b353eb374a664f2ed59
Anthony Quach <ta.quach@hotmail.com>
2025-01-24 12:00:37 +0100

    merge fix

---
+++ Assets/Scripts/Map/Generation/Biome.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biome 
+{
+    Dictionary<Vector2, TerrainChunk> terrainChunkDict = new Dictionary<Vector2, TerrainChunk>();
+
+
+
+    public Biome(Vector2 chunkCoord, TerrainChunk chunk)
+    {
+
+    }
+
+
+    void GenerateBiome()
+    {
+
+    }
+}
+
